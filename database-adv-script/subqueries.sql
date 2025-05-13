28f9ac5a47487a7fee1b495edde12ea72c73f7c9
Achiugo Trust <achiugotrust@gmail.com>
2025-05-13 14:17:54 +0100

    subqueries

---
+++ database-adv-script/subqueries.sql
@@ -0,0 +1,13 @@
+/*correlated subquery to find users who have made more than 3 bookings.*/
+
+SELECT * FROM property
+WHERE property_id IN
+(SELECT property_id FROM review
+GROUP BY property_id
+HAVING AVG(rating) > 4.0)
+
+/*correlated subquery to find users who have made more than 3 bookings.*/
+SELECT * from users
+WHERE user_id in (SELECT user_id FROM booking
+GROUP BY user_id
+HAVING count(booking_id) > 3);
