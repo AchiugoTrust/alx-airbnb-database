0151aaa866dbca7cc2023f091801f7fd32365793
Achiugo Trust <achiugotrust@gmail.com>
2025-05-13 16:15:05 +0100

    aggregations and window functions

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -7,6 +7,6 @@ GROUP BY user_id;
 /*window function to rank properties based on the total number of bookings they have received.*/
 SELECT property_id, 
        COUNT(booking_id) AS total_bookings,
-       RANK() OVER (ORDER BY COUNT(booking_id) DESC) AS ranking
+       ROW_NUMBER() OVER (ORDER BY COUNT(booking_id) DESC) AS ranking
 FROM booking
 GROUP BY property_id;
